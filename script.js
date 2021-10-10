a31f8b4fa64bdf5ccb633e3fb8e6c63117830d15
Tan Duong <dntan09@gmail.com>
2021-10-10 17:27:08 +0700

    feat: alert on game finished

--- script.js
+++ script.js
@@ -71,10 +71,94 @@ document.querySelector("body").addEventListener("click", (event) => {
     console.log("Click!", x, y);
 
     gameState[x][y] = activePlayer;
-    activePlayer = 3 - activePlayer;
 
     render();
+
+    const jugdement = isFinished(gameState);
+
+    if (jugdement === X) {
+      alert("X won!");
+    }
+
+    if (jugdement === O) {
+      alert("O won!");
+    }
+
+    if (jugdement === 0) {
+      activePlayer = 3 - activePlayer;
+    }
+
+    if (jugdement === 3) {
+      alert("Draw!");
+    }
   }
 });
 
 // Event delegation
+
+function rowWithSameValue(gameState, rowIndex, value) {
+  return gameState[rowIndex].every((x) => x === value);
+}
+
+function columnWithSameValue(gameState, colIndex, value) {
+  return gameState.every((row) => row[colIndex] === value);
+}
+
+function anyDiagWithSameValue(gameState, value) {
+  const diag1 = [gameState[0][0], gameState[1][1], gameState[2][2]];
+  const diag2 = [gameState[0][2], gameState[1][1], gameState[2][0]];
+
+  return diag1.every((x) => x === value) || diag2.every((x) => x === value);
+}
+
+function anyRowWithSameValue(gameState, value) {
+  return [0, 1, 2].some((rowIndex) =>
+    rowWithSameValue(gameState, rowIndex, value)
+  );
+}
+
+function anyColWithSameValue(gameState, value) {
+  return [0, 1, 2].some((colIndex) =>
+    columnWithSameValue(gameState, colIndex, value)
+  );
+}
+
+function anyLineWithValue(gameState, value) {
+  return (
+    anyRowWithSameValue(gameState, value) ||
+    anyColWithSameValue(gameState, value) ||
+    anyDiagWithSameValue(gameState, value)
+  );
+}
+
+function full(gameState) {
+  return gameState.every((row) => row.every((cell) => cell !== 0));
+}
+
+function isFinished(gameState) {
+  if (anyLineWithValue(gameState, X)) {
+    return 1;
+  }
+
+  if (anyLineWithValue(gameState, O)) {
+    return 2;
+  }
+
+  if (full(gameState)) {
+    return 3;
+  }
+
+  return 0;
+}
+
+console.assert(
+  anyLineWithValue(
+    [
+      [1, 0, 0],
+      [0, 1, 1],
+      [1, 0, 1],
+    ],
+    1
+  ),
+  "has 1 row x"
+);
